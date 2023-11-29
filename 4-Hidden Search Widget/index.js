cc00e4f70a39aa572c90d43450fecb86beadfee8
Kuldeep Yadav <64552714+yadav4646@users.noreply.github.com>
2023-11-29 16:14:06 +0530

    Add files via upload

---
+++ 4-Hidden Search Widget/index.js
@@ -0,0 +1,9 @@
+var search = document.querySelector(".search");
+var btn = document.querySelector(".btn");
+var input = document.querySelector(".input");
+
+
+btn.addEventListener('click', () => {
+    search.classList.toggle('active');
+    input.focus();
+})
