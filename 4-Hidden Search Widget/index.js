a51a7550f2eaabe2c4e840af6bb27e561d2ea452
Kuldeep Yadav <64552714+yadav4646@users.noreply.github.com>
2023-11-29 16:20:04 +0530

    Delete 4-Hidden Search Widget directory

--- 4-Hidden Search Widget/index.js
+++
@@ -1,9 +0,0 @@
-var search = document.querySelector(".search");
-var btn = document.querySelector(".btn");
-var input = document.querySelector(".input");
-
-
-btn.addEventListener('click', () => {
-    search.classList.toggle('active');
-    input.focus();
-})
