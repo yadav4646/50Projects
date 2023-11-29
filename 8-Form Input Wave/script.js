5bf5492f69aa6e8a08b614b07f6860ff258dbd77
Kuldeep Yadav <64552714+yadav4646@users.noreply.github.com>
2023-11-29 16:20:32 +0530

    Delete 8-Form Input Wave directory

--- 8-Form Input Wave/script.js
+++
@@ -1,8 +0,0 @@
-const label = document.querySelectorAll(".form-control label")
-
-label.forEach(label => {
-    label.innerHTML = label.innerText
-        .split('')
-        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
-        .join('')
-})
